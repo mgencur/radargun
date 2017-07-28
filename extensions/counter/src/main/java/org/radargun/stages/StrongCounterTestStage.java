4468af1937c6b324cfc706a209c275440eef2067
Martin Gencur <mgencur@redhat.com>
2017-07-28 14:06:28 +0200

    Add logic for StrongCounters compareAndSet operation

--- extensions/counter/src/main/java/org/radargun/stages/StrongCounterTestStage.java
+++ extensions/counter/src/main/java/org/radargun/stages/StrongCounterTestStage.java
@@ -56,7 +56,12 @@ public class StrongCounterTestStage extends TestStage {
    }
 
    /**
-    *
+    * Stressor logic for strong counter operations. All operations except COMPARE_AND_SET
+    * are supposed to use a shared counter between threads (this tests both consistency and
+    * performance). The COMPARE_AND_SET logic is supposed to
+    * use a different counter for each thread otherwise the first thread that updates the value
+    * prevents all the other threads from ever updating (succeeding) the value because the "compare" operation will
+    * always fail for them (this tests only performance, not consistency as the counters are not shared).
     */
    protected class StrongCounterLogic extends OperationLogic {
       private StrongCounterOperations.StrongCounter counter;
@@ -75,36 +80,35 @@ public class StrongCounterTestStage extends TestStage {
 
       @Override
       public void run(Operation operation) throws RequestException {
+         long currentValue;
          if (operation == StrongCounterOperations.INCREMENT_AND_GET) {
             Invocation<Long> invocation = new StrongCounterInvocations.IncrementAndGet(counter);
-            long currentValue = stressor.makeRequest(invocation);
-            if (currentValue == previousValue) {
-               throw new IllegalStateException("Inconsistent counter! The value should be different from "
-                  + previousValue);
-            } else {
-               previousValue = currentValue;
-            }
+            currentValue = stressor.makeRequest(invocation);
+            assertNotEqual(previousValue, currentValue);
          } else if (operation == StrongCounterOperations.DECREMENT_AND_GET) {
             Invocation<Long> invocation = new StrongCounterInvocations.DecrementAndGet(counter);
-            long currentValue = stressor.makeRequest(invocation);
-            if (currentValue == previousValue) {
-               throw new IllegalStateException("Inconsistent counter! The value should be different from "
-                  + previousValue);
-            } else {
-               previousValue = currentValue;
-            }
+            currentValue = stressor.makeRequest(invocation);
+            assertNotEqual(previousValue, currentValue);
          } else if (operation == StrongCounterOperations.ADD_AND_GET) {
             Invocation<Long> invocation = new StrongCounterInvocations.AddAndGet(counter, delta);
-            long currentValue = stressor.makeRequest(invocation);
-            if (currentValue == previousValue) {
-               throw new IllegalStateException("Inconsistent counter! The value should be different from "
-                  + previousValue);
-            } else {
-               previousValue = currentValue;
-            }
+            currentValue = stressor.makeRequest(invocation);
+            assertNotEqual(previousValue, currentValue);
          } else if (operation == StrongCounterOperations.COMPARE_AND_SET) {
-               throw new RuntimeException("Operation not implemented!");
+            currentValue = previousValue + 1;
+            Invocation<Boolean> invocation = new StrongCounterInvocations.CompareAndSet(counter, previousValue, currentValue);
+            boolean success = stressor.makeRequest(invocation);
+            if (!success) {
+               throw new IllegalStateException("Inconsistent counter! The value should be " + previousValue);
+            }
          } else throw new IllegalArgumentException(operation.name);
+
+         previousValue = currentValue; // exception was NOT thrown so we can update previous value for all operations
+      }
+
+      private void assertNotEqual(long previous, long current) {
+         if (previous == current) {
+            throw new IllegalStateException("Inconsistent counter! The value should be different from " + previous);
+         }
       }
    }
 }
